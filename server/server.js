acac3eb78521ae83525f7e132e1c29167a2b341c
Berrydoo <Jim.dewberry@Gmail.com>
2016-11-09 22:32:43 -0500

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (newMessage) => {
         console.log('createMessage', newMessage);
 
@@ -21,7 +33,14 @@ io.on('connection', (socket) => {
             from: newMessage.from,
             text: newMessage.text,
             createdAt: new Date().getTime()
-        })
+        });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: newMessage.from,
+        //     text: newMessage.text,
+        //     createdAt: new Date().getTime()
+        // })
+
     });
 
     socket.on('disconnect', (socket) => {
